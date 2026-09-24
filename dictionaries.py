2aebc88b38cf3e0afdf6e822914cd7471a3e5791
ropykevin <ropykevin@gmail.com>
2026-09-24 04:24:45 -0900

    final ds

--- dictionaries.py
+++ dictionaries.py
@@ -9,8 +9,10 @@ student1={
     "age":21,
     "country":"Kenya",
     "gender":'Male',
+    "hobbies":['Painting','Writing','Hiking']
 }
 print(student1['country'])
+print(student1.get('country'))
 # add and update properties
 student1['city']='Nairobi'
 print(student1)
@@ -19,3 +21,23 @@ print(student1)
 student1['age']=30
 print(student1)
 # update name
+# add a new key skills with values ["Web dev","UI",UX]
+student1['skills']=["Web dev","UI",'UX']
+print(student1['skills'][1])
+print(student1["hobbies"][2])
+
+student1['hobbies'].remove('Hiking')
+print(student1)
+
+del(student1['gender'])
+print(student1)
+
+# keys
+print(student1.keys())
+# values
+print(student1.values())
+# items
+print(student1.items())
+
+#get
+print(student1.get('age'))
