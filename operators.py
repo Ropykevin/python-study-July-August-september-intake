1dfa080416a74be8fb671e0ab8e5f751d4c21a63
ropykevin <ropykevin@gmail.com>
2026-09-25 07:49:16 -0900

    first commit

---
+++ operators.py
@@ -0,0 +1,27 @@
+# Operators 
+# =>used to perfom programming operations
+        # categories of operators 
+# 1.Arithmetic operators->used for calculations (+,**,*,/,%,//,-)
+
+# 2.Comparison operators->used to compare values or variables(==,>,<,>=,<=,!=)
+# return Booleans
+print(10!=11)
+
+# Logical operators =>used to combine conditions
+# and->returns True when all conditions are true 
+print(10<20 and 250<500)
+# or ->returns true if one of the conditions is met
+print(10 > 20 or 250 <500)
+# Not reverses the result
+print(not 10<5)
+
+# Assignment Operators->used to assign values to Variables(=,+=,-=,*=,/=)
+x=20
+y=30
+x=x+y
+print(x)
+
+# conditional statement
+# loops
+# Functions
+# Classes(oop)-> flask 
---
+++ operators1.py
@@ -0,0 +1,46 @@
+# introduction to python
+# Inbuilt functions(print(),len(),input,(),type(),round(),float(),int(),str(),list(),tuple(),set(),del())
+# Variables
+# Datatypes(strings,integers,floats,Booleans)
+# Data structures(Dictionaries,lists,tuples,sets)
+# operators
+# conditional statements
+# loops
+# functions
+# OOP
+
+
+            # operators
+# =>Special symbols or keywords used to perfom operations on values and Variables 
+    # Categories of operators
+# -> Arithmetic Operators ->used to perfom calculations(%,//,-,**,+,*,/)
+# 
+print(11%2)
+print(11//2)
+print(11**2)
+# ->Comparison operators ->Used to Compare values or Variables(>,<,==,>=,!=,<=)
+# They return Boolean (True or False)
+# create conditions
+print(10==10)
+
+# ->Logical Operators -> Used to combine conditions
+# and ->returns True if all conditions are true
+
+print(20<10 and 40<50)
+
+# or ->Returns True if atleast one condition is true
+print(20 < 10 or 40 <50)
+
+# Not ->Inverts the result of a condition
+
+print(not 10>20)
+
+# ->Assignment Operators ->Used to assign Values to Variables (=,+=,-=,/=)
+
+x=20
+y=30
+
+x=x/y
+# x/=y
+
+print(x)
